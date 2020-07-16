7ab6293fb5deb6d8f2f66c3eda09ba658dd08863
unknown <raihanshovon10@gmail.com>
2020-07-16 12:28:18 +0600

    Banner complete

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    // autoplay: true,
+    autoplayTimeout: 4000,
+    smartSpeed: 1000,
+    animateIn: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
