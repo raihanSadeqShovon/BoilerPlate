7bb25f7310922737d555df897a587edd685dff08
Raihan Sadeq <raihanshovon@yahoo.com>
2020-10-20 18:43:00 +0600

    Preloader added

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,16 +71,12 @@ $(function () {
 
 // Preloader
 $(function () {
-  $.scrollUp({
-    scrollName: 'scrollUp', // Element ID
-    topDistance: '300', // Distance from top before showing element (px)
-    topSpeed: 300, // Speed back to top (ms)
-    animation: 'fade', // Fade, slide, none
-    animationInSpeed: 200, // Animation in speed (ms)
-    animationOutSpeed: 200, // Animation out speed (ms)
-    scrollText: 'Scroll to top', // Text for element
-    activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-  });
+
+  $(window).ready(function () {
+    setTimeout(function(){
+      $(".preLoader").fadeOut();
+    },500);
+});
 });
 
 
